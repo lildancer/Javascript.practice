3e51ea77ebd0c9df349f3d45d503a0c7dfe847c7
lildancer <littlecarmel3@gmail.com>
2017-07-25 09:28:29 -0600

    Add files via upload
    copied and modified javascript warmup from 7.25.17

---
+++ app.2.js
@@ -0,0 +1,11 @@
+var carmel= 20;
+if (carmel >= 16) {return true} else {return false}
+
+function discount(total, discount){
+   return  (total - discount=num) + num.value= "$" + num.toFixed(2)
+}
+
+total = 20;
+discount = 2.50;
+
+function discount (20,2.50);
